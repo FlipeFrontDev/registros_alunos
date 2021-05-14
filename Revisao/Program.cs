421fc6afbc471313b4af5d408e40732a0fc53c42
Felipe Moreira <felipe.saint@gmail.com>
2021-05-14 19:35:42 -0300

    Desenvolvimento do modulo de alunos

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -2,108 +2,143 @@
 
 namespace Revisao
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Aluno[] alunos = new Aluno[5];
-            var indiceAluno = 0;
-            string opcaoUsuario = ObterOpcaoUsuario();
+      //cria vetor alunos do tipo Aluno
 
-            while (opcaoUsuario.ToUpper() != "X")
+      Aluno[] alunos = new Aluno[5];
+      var indiceAlunos = 0;
+
+      string opcaoUsuario = OpcaoUsuario();
+
+      while (opcaoUsuario.ToUpper() != "X")
+      {
+
+        switch (opcaoUsuario)
+        {
+
+          case "1":
+
+            Console.WriteLine("Informe o nome do aluno: ");
+
+            Aluno aluno = new Aluno();
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a nota do aluno: ");
+
+            if (decimal.TryParse(Console.ReadLine(), out decimal nota))
             {
-                switch (opcaoUsuario)
-                {
-                    case "1":
-                        Console.WriteLine("Informe o nome do aluno:");
-                        var aluno = new Aluno();
-                        aluno.Nome = Console.ReadLine();
 
-                        Console.WriteLine("Informe a nota do aluno:");
+              aluno.Nota = nota;
 
-                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
-                        {
-                            aluno.Nota = nota;
-                        }
-                        else
-                        {
-                            throw new ArgumentException("Valor da nota deve ser decimal");
-                        }
-
-                        alunos[indiceAluno] = aluno;
-                        indiceAluno++;
-
-                        break;
-                    case "2":
-                        foreach (var a in alunos)
-                        {
-                            if (!string.IsNullOrEmpty(a.Nome))
-                            {
-                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
-                            }
-                        }
-                        break;
-                    case "3":
-                        decimal notaTotal = 0;
-                        var nrAlunos = 0;
-
-                        for (int i=0; i < alunos.Length; i++)
-                        {
-                            if (!string.IsNullOrEmpty(alunos[i].Nome))
-                            {
-                                notaTotal = notaTotal + alunos[i].Nota;
-                                nrAlunos++;
-                            }
-                        }
-
-                        var mediaGeral = notaTotal / nrAlunos;
-                        Conceito conceitoGeral;
-
-                        if (mediaGeral < 2)
-                        {
-                            conceitoGeral = Conceito.E;
-                        }
-                        else if (mediaGeral < 4)
-                        {
-                            conceitoGeral = Conceito.D;
-                        }
-                        else if (mediaGeral < 6)
-                        {
-                            conceitoGeral = Conceito.C;
-                        }
-                        else if (mediaGeral < 8)
-                        {
-                            conceitoGeral = Conceito.B;
-                        }
-                        else
-                        { 
-                            conceitoGeral = Conceito.A;
-                        }
-
-                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
-
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                opcaoUsuario = ObterOpcaoUsuario();
             }
-        }
+            else
+            {
 
-        private static string ObterOpcaoUsuario()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Calcular média geral");
-            Console.WriteLine("X- Sair");
-            Console.WriteLine();
+              throw new ArgumentException("Valor da nota deve ser decimal!");
 
-            string opcaoUsuario = Console.ReadLine();
-            Console.WriteLine();
-            return opcaoUsuario;
+            }
+
+            alunos[indiceAlunos] = aluno;
+
+            indiceAlunos++;
+
+            break;
+
+          case "2":
+
+            foreach (var a in alunos)
+            {
+
+              if (!string.IsNullOrEmpty(a.Nome))
+              {
+
+                Console.WriteLine($"ALUNO = {a.Nome} - NOTA = {a.Nota}");
+
+              }
+            }
+
+            break;
+
+          case "3":
+
+            decimal media = 0;
+            int qtdeAlunos = 0;
+            Conceito conceito;
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+              if (!string.IsNullOrEmpty(alunos[i].Nome))
+              {
+
+                media += alunos[i].Nota;
+                qtdeAlunos++;
+
+              }
+            }
+
+            media /= qtdeAlunos;
+
+            if (media < 2)
+            {
+
+              conceito = Conceito.E;
+
+            }
+            if (media < 4)
+            {
+
+              conceito = Conceito.D;
+
+            }
+            if (media < 6)
+            {
+
+              conceito = Conceito.C;
+
+            }
+            if (media < 8)
+            {
+
+              conceito = Conceito.B;
+
+            }
+            else
+            {
+
+              conceito = Conceito.A;
+
+            }
+
+            Console.WriteLine($"MÉDIA = {media} - CONCEITO = {conceito}");
+
+            break;
+
+          default:
+
+            throw new ArgumentOutOfRangeException();
+
         }
+        opcaoUsuario = OpcaoUsuario();
+      }
     }
+
+    private static string OpcaoUsuario()
+    {
+      Console.WriteLine();
+      Console.WriteLine("Informe a opção desejada: ");
+      Console.WriteLine("1 - Inserir novo aluno");
+      Console.WriteLine("2 - Listar alunos");
+      Console.WriteLine("3 - Calcular média geral");
+      Console.WriteLine("X: - Sair");
+
+      Console.WriteLine();
+
+      string opcao = Console.ReadLine();
+      Console.WriteLine();
+      return opcao;
+    }
+  }
 }
